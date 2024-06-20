c85066a1087ab324046db17df996b22b5b710b57
Theecanmole <theecanmole@gmail.com>
2024-06-20 17:59:53 +1200

    update

--- Readme.md
+++ Readme.md
@@ -1,6 +1,36 @@
 ## How many emission units have been allocated to New Zealand Aluminium Smelters Limited under the New Zealand Emissions Trading Scheme? 
 
-### Description
+How many emission units have been allocated to New Zealand Aluminium Smelters Limited under the New Zealand Emissions Trading Scheme? What was their market value when they were allocated? What is the total value of the units given since 2010?
+
+Follow the steps in the R script file Sum-allocation-2010-2018.r
+
+This table sets out the final allocation of emission units allocated each year, the May market price and an estimated market value at the May price (based on the assumption that almost all of the units are transferred at the time of the provisional allocation of the current year which is also the 'wash-up' of the prior years final allocation).
+
+|   Year |Final units allocated | May unit value | Total value |
+|----	 |----        |----   |----     |	
+|  2010  |    210,421  | 17.6  | 3,699,201|
+|   2011 |    437,681  | 19.8  | 8,683,591|
+|   2012 |    301,244  |  6.23 | 1,876,750|
+|   2013 |  1,524,172  |  1.94 | 2,956,894|
+|   2014 |    755,987  |  4.08 | 3,084,427|
+|   2015 |    772,706  |  5.34 | 4,126,250|
+|   2016 |    786,306  | 14.6  |11,503,657|
+|   2017 |  1,038,914  | 17.0  |17,619,981|
+|   2018 |  1,324,556  | 21.3  |28,186,552|
+|  2019  |  1,697,437  | 25.3  |42,928,182|
+|  2020  |  1,558,268  | 24.8  |38,707,377|
+|  2021  |    628,561  | 37.1  |23,344,756|
+|  2022  |    605,320  | 76.6  |46,337,246|
+|TOTAL	 | 11,641,573  |N/A    |233 054,863|
+
+![](EPA-letter-provisional-allocation.png)
+
+
+![Quantity of NZETS Emission Units Allocated to NZ Aluminium Smelters Ltd from 2010](NZAL-2010-2020-allocations_720-540font11.svg)
+
+![Value of NZETS Emission Units Allocated to NZ Aluminium Smelters Ltd from 2010](NZAL-allocation-value-720-540f12.svg)
+
+### Background to free allocation of emission units
 
 The New Zealand [Environmental Protection Authority](https://www.epa.govt.nz) manages the [New Zealand Emissions Trading Scheme](https://www.epa.govt.nz/industry-areas/emissions-trading-scheme/). 
 
@@ -16,41 +46,15 @@ The EPA website states at; [EPA application deadlines](https://www.epa.govt.nz/i
 
 The EPA website also discloses a complete record of all industry final allocations in a MS Excel workbook [Industrial-Allocations-Final-Decisions.xlsx](https://www.epa.govt.nz/assets/Uploads/Documents/Emissions-Trading-Scheme/Reports/Industrial-Allocations/Industrial-Allocations-Final-Decisions.xlsx)
 
-How many emission units have been allocated to New Zealand Aluminium Smelters Limited under the New Zealand Emissions Trading Scheme? What was their market value when they were allocated? What is the total value of the units given since 2010?
-
-Follow the steps in the R script file Sum-allocation-2010-2018.r
-
-This table sets out the final allocation of emission units allocated each year, the May market price and an estimated market value at the May price (based on the assumption that almost all of the units are transferred at the time of the provisional allocation of the current year which is also the 'wash-up' of the prior years final allocation).
-
-|   Year |Final units allocated | May unit value | Total value |
-|----	 |----       |---- 	 |----     |	
-|  2010  |   210,421  | 17.6  | 3,699,201|
-|   2011 |   437,681  | 19.8  | 8,683,591|
-|   2012 |   301,244  |  6.23 | 1,876,750|
-|   2013 |  1,524,172  |  1.94 | 2,956,894|
-|   2014 |   755,987  |  4.08 | 3,084,427|
-|   2015 |   772,706  |  5.34 | 4,126,250|
-|   2016 |   786,306  | 14.6  |11,503,657|
-|   2017 |  1,038,914  | 17.0  |17,619,981|
-|   2018 |  1,324,556  | 21.3  |28,186,552|
-|  2019  |  1,697,437  | 25.3  |42,928,182|
-|  2020  |  1,558,268  | 24.8  |38,707,377|
-|  2021  |    628,561  | 37.1  |23,344,756|
-|  2022  |    605,320  | 76.6  |46,337,246|
-|TOTAL	 | 11,641,573  |N/A    |233 054,863|
-
 ### Provisional allocations
+
 Each year, from 1 May, the EPA makes a [provisional allocation](https://www.epa.govt.nz/industry-areas/emissions-trading-scheme/industrial-allocations/) of emission units to selected industries. I want to estimate the market value of each years free allocation of units. I understand that the deadline for a provisional allocation is 30 April of each year so I assume the transfer of the emission units is made in May of each year. I assume that the provisional allocation for the current year (and the wash-up calculation of the final allocation for the previous year) are transferred to the emitter in May.
 
 ### Average May emission unit price
-There is an online 'open data' Github repository of New Zealand Unit (NZU) prices going back to May 2010. This data set has it's own citation and DOI: Theecanmole. (2016). [New Zealand emission unit (NZU) monthly prices 2010 to 2016](https://github.com/theecanmole/nzu) [V1.0.01 [Data set]. Zenodo](http://doi.org/10.5281/zenodo.221328). I add a market price for the units at the May average price from 2010 to 2020 to the annual allocation data. 
 
-How do I estimate the 2021 provisional allocation that was probably processed by EPA in May 2021? It is based on prior year 2020 actual production see [Section 81(1) of the Climate Change Response Act 2002](https://www.legislation.govt.nz/act/public/2002/0040/latest/DLM1662643.html). So obtain the 2020 final allocation of units and divide by 2020 final allocation baseline from [Regulation 7 of the Climate Change (Eligible Industrial Activities) Regulations 2010](https://www.legislation.govt.nz/regulation/public/2010/0189/latest/DLM3075118.html). The final 2020 allocation of units multiplied by the 2021 allocation baseline equals the 2021 provisional allocation. 
+There is an online 'open data' Github repository of New Zealand Unit (NZU) prices going back to May 2010. This data set has it's own citation and DOI: Theecanmole. (2016). [New Zealand emission unit (NZU) monthly prices](https://github.com/theecanmole/nzu) [V1.0.01 [Data set]. Zenodo](http://doi.org/10.5281/zenodo.221328). I add a market price for the units at the May average price from 2010 to 2020 to the annual allocation data. 
 
-![Quantity of NZETS Emission Units Allocated to NZ Aluminium Smelters Ltd from 2010](NZAL-2010-2020-allocations_720-540font11.svg)
-
-![Value of NZETS Emission Units Allocated to NZ Aluminium Smelters Ltd from 2010](NZAL-allocation-value-720-540f12.svg)
-
+How do I estimate the 2021 provisional allocation that was probably processed by EPA in May 2021? It is based on prior year 2020 actual production see [Section 81(1) of the Climate Change Response Act 2002](https://www.legislation.govt.nz/act/public/2002/0040/latest/DLM1662643.html). So obtain the 2020 final allocation of units and divide by 2020 final "allocative baseline" from [Regulation 7 of the Climate Change (Eligible Industrial Activities) Regulations 2010](https://www.legislation.govt.nz/regulation/public/2010/0189/latest/DLM3075118.html). The final 2020 allocation of units multiplied by the 2021 allocation baseline equals the 2021 provisional allocation. 
 
 ### License
 
--- Sum-allocation-2010-2018.r
+++ Sum-allocation-2010-2018.r
@@ -1,13 +1,4 @@
-# NZ Aluminium free industrial allocation of emissions units 2010 to 2022
-
-https://www.epa.govt.nz/industry-areas/emissions-trading-scheme/industrial-allocations/how-to-apply/    Step 2 choose an application type
-#Provisional If you apply for a Provisional Allocation you receive your entitlement in advance, based on your production for the previous calendar year. To ‘square up’ your entitlement with what you actually produced, in the next application period you’re required to submit an allocation adjustment.
-
-https://www.epa.govt.nz/industry-areas/emissions-trading-scheme/industrial-allocations/how-to-apply/    correct at 27/02/2022
-#Deadlines  info@epa.govt.nz
-#You must submit your application via the Register by the following statutory deadlines:
-#Provisional allocation applications: Apply between 1 January - 30 April of the year for which you wish to receive NZUs.
-#Final allocation applications and annual allocation adjustments: Apply between 1 January - 30 April of the year following the year for which you wish to receive NZUs.
+...## NZ Aluminium free industrial allocation of emissions units from 2010
 
 # Industry final allocations to 2022
 https://www.epa.govt.nz/assets/Uploads/Documents/Emissions-Trading-Scheme/Reports/Industrial-Allocations/Industrial-Allocations-Final-Decisions_2022.xlsx
@@ -99,13 +90,11 @@ nzu2022 <- filter(Allocations, Year =="2022")
 
 # check just the 2010 data
 str(nzu2010)
-
 tibble [141 × 4] (S3: tbl_df/tbl/data.frame)
  $ Activity : chr [1:141] "Aluminium smelting" "Burnt lime" "Burnt lime" "Burnt lime" ...
  $ Applicant: chr [1:141] "New Zealand Aluminium Smelters Limited" "Graymont (NZ) Limited" "Holcim (New Zealand) Limited" "Perry Resources (2008) Ltd" ...
  $ Year     : num [1:141] 2010 2010 2010 2010 2010 2010 2010 2010 2010 2010 ...
  $ Units    : num [1:141] 210421 47144 3653 4712 948 ...
-
  
 # Each year, from May, the EPA makes a 'provisional' allocation of emssion units to selected industries. see https://www.epa.govt.nz/industry-areas/emissions-trading-scheme/industrial-allocations/ I want to estimate the market value of free allocation of units. I understand that the deadline for a provisional allocation is 30 April of each year so I assume the transfer of allocation is made in May of each year. 
 # There is an online 'open data' Github repository of New Zealand Unit (NZU) prices going back to May 2010. https://github.com/theecanmole/nzu, the NZU repository has it's own citation and DOI: Theecanmole. (2016). New Zealand emission unit (NZU) monthly prices 2010 to 2016: V1.0.01 [Data set]. Zenodo. http://doi.org/10.5281/zenodo.221328
@@ -150,7 +139,6 @@ Allocations$Unitvalue = Allocations[["Units"]]*Allocations[["Mayunitprice"]]
 
 summary(Allocations$Unitvalue) 
 
-   
 # Create a .csv formatted data file
 write.csv(Allocations, file = "Allocations.csv", row.names = FALSE)
 
@@ -159,9 +147,6 @@ Allocations <- read.csv("Allocations.csv")
 
 # Create a Windows Excel 2007/10 formatted data file (if needed)
 write.csv(Allocations, file = "Allocations.xls", row.names = FALSE, fileEncoding = "UTF-16LE") 
-
-# read back in the new 2010 2020 data if needed
-Allocations <- read.csv("Allocations.csv") 
 
 str(Allocations)
 tibble [1,377 × 6] (S3: tbl_df/tbl/data.frame)
@@ -172,8 +157,7 @@ tibble [1,377 × 6] (S3: tbl_df/tbl/data.frame)
  $ Mayunitprice: num [1:1377] 17.6 17.6 17.6 17.6 17.6 ...
  $ Unitvalue   : num [1:1377] 3699201 828792 64220 82837 16666 ...
 
- 
- # look at summary of allocations dataframe
+# look at summary of allocations dataframe
 summary(Allocations)
    Activity          Applicant              Year          Units        
  Length:1377        Length:1377        Min.   :2010   Min.   :      1  
@@ -203,11 +187,10 @@ tibble [13 × 6] (S3: tbl_df/tbl/data.frame)
  $ Mayunitprice: num [1:13] 17.58 19.84 6.23 1.94 4.08 ...
  $ Unitvalue   : num [1:13] 3699201 8683591 1876750 2956894 3084427 ...
 
-
 # filter out redundant columns to leave year allocation May price and value
 nzal <- select(nzal, -Activity, -Applicant)
 
-# what is the most recent year of allocation? It is 2022. 
+# what is the most recent year of Final allocation? It is 2022. 
 tail(nzal)
 # A tibble: 6 × 4
    Year   Units Mayunitprice Unitvalue
@@ -249,6 +232,8 @@ nzal
 1539067 * 37.14
 [1] 57160948 
 
+ls()
+
 # add provisional 2021 allocation to data frame 'nzal'
 #nzal <- rbind (nzal,c(2021,1539067,57160948))
 
@@ -264,18 +249,37 @@ tibble [13 × 4] (S3: tbl_df/tbl/data.frame)
 write.csv(nzal, file = "nzal.csv", row.names = FALSE)
 
 # read my csv data file back into R
-#nzal <- read.csv("nzal.csv") 
+nzal <- read.csv("nzal.csv") 
+# add 'Final' allocation baseline factor
+nzal$Baseline <- baselines[1:13] 
+str(nzal)
+'data.frame':	13 obs. of  5 variables:
+ $ Year        : int  2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 ...
+ $ Units       : int  210421 437681 301244 1524172 755987 772706 786306 1038914 1324556 1697437 ...
+ $ Mayunitprice: num  17.58 19.84 6.23 1.94 4.08 ...
+ $ Unitvalue   : num  3699201 8683591 1876750 2956894 3084427 ...
+ $ Baseline    : num  2.65 2.73 2.06 10.44 5.14 ... 
+
+ # add aluminium production based on P x B = U or P = U / B
+nzal$Production <- nzal[["Units"]]/nzal[["Baseline"]] 
+'data.frame':	13 obs. of  6 variables:
+ $ Year        : int  2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 ...
+ $ Units       : int  210421 437681 301244 1524172 755987 772706 786306 1038914 1324556 1697437 ...
+ $ Mayunitprice: num  17.58 19.84 6.23 1.94 4.08 ...
+ $ Unitvalue   : num  3699201 8683591 1876750 2956894 3084427 ...
+ $ Baseline    : num  2.65 2.73 2.06 10.44 5.14 ...
+ $ Production  : num  79554 160558 146093 145980 147194 ... 
+ 
 str(nzallist) 
-'data.frame':	12 obs. of  3 variables:
- $ Year      : int  2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 ...
- $ Allocation: int  210421 437681 301244 1524172 755987 772706 786306 1038914 1324556 1697437 ...
- $ Value     : num  3699201 8683591 1876750 2956894 3084427 
+
+
+
 dump("nzal", file = "nzaldumpdata.R", append = FALSE,control = "all", envir = parent.frame(), evaluate = TRUE)
 save.image() 
 
 # How many NZUs were given to  New Zealand Aluminium Smelters Limited in total?
 sum(nzal[["Units"]]) 
-[1] 11641573
+[1] 11641573            # 12 million!
 
 # [1] 11946759
 # 11,946,759 or 11.946759 million emission units  or call it 12 million
@@ -304,8 +308,9 @@ select(nzal,c(Year, Units))
 12  2021     628561
 13  2022     605320
 # create plot
-plot(select(nzal,c(Year, Units)),type="s",lwd=3,col=4) 
-
+max(nzal$Units/10^6) # [1] 1.697437
+plot(select(nzal,c(Year, Units/10^6)),type="o",lwd=3,col=4) 
+plot(nzal$Year, nzal$Units/10^6,ylim=c(0,2),las=1,type="o",lwd=3,col=4) 
 # edit the unit allocations into a matrix, create a numeric vector of NZAL allocations
 a <-nzal$Units
 str(a) 
@@ -393,5 +398,163 @@ mtext(side=3,line=0.25,cex=1,expression(paste("The 2021 final allocative baselin
 mtext(side=1,line=2.5,cex=1,expression(paste("Data: Climate Change (Eligible Industrial Activities) Regulations 2010")))
 dev.off()
 
+==================================================================================
 
 https://www.legislation.govt.nz/regulation/public/2010/0189/latest/DLM3075118.html
+
+# create table that is NZ AL allocation baseline Climate Change (Eligible Industrial Activities) Regulations 2010 No 7
+# https://www.legislation.govt.nz/regulation/public/2010/0189/latest/DLM3075118.html
+
+# read in the Final baselines from the csv file copied from the legislation web page
+baseline <- read.csv("baseline.csv") 
+baseline 
+   Allocativebaseline Year
+1               2.645 2010
+2               2.726 2011
+3               2.062 2012
+4              10.441 2013
+5               5.136 2014
+6               5.152 2015
+7               5.160 2016
+8               5.142 2017
+9               5.184 2018
+10              5.366 2019
+11              5.194 2020
+12              2.120 2021
+13              2.048 2022
+14              2.042 2023 
+
+baselines <-c(baseline$Allocativebaseline)
+
+str(baselines)
+ num [1:14] 2.65 2.73 2.06 10.44 5.14 ...
+
+nzalbaseline <- matrix(baselines, nrow = 1, ncol=14, byrow=TRUE, dimnames = list(c("NA"),
+c("2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022","2023")))
+
+svg(filename ="NZAL-Allocation-baseline-720-540.svg", width = 8, height = 6, pointsize = 12, onefile = FALSE, family = "sans", bg = "white")
+#png("NZAL-Allocation-baseline-560by420-v1.png", bg="white", width=560, height=420,pointsize = 11)
+par(mar=c(4, 4, 4, 1)+0.1)
+barplot(nzalbaseline,ylim=c(0,11),las=1,space=c(0.1,1.1), beside = TRUE, col=c(rep("#ED731D",14),"red"))
+title(cex.main=1.4,main=expression(paste("Aluminium final allocation baseline factor 2010 - 2023")),ylab="Units per tonne aluminium produced",xlab="")
+mtext(side=1,line=2.5,cex=1,expression(paste("Source: Climate Change (Eligible Industrial Activities) Regulations 2010 No 7")))
+mtext(side=3,line=0,cex=0.9,expression(paste("What happened in 2013? The allocation factor is five times more than emissions per tonne aluminium")))
+legend("topright", inset=c(0.0,0.0) ,bty="n",cex=1.2,c("Final allocation","Provisional allocation"),fill=c("#ED731D","red"))
+dev.off()
+
+# add 2024 provisional baseline
+baseline1 <- rbind(baseline,c(2.042,2024))
+str(baseline1) 
+'data.frame':	15 obs. of  2 variables:
+ $ Allocativebaseline: num  2.65 2.73 2.06 10.44 5.14 ...
+ $ Year              : num  2010 2011 2012 2013 2014 ...
+
+# create matrix
+nzalbaseline1 <- matrix(baseline1$Allocativebaseline, nrow = 1, ncol=15, byrow=TRUE, dimnames = list(c("NA"),
+c("2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022","2023","2024")))
+
+nzalbaseline1 
+    2010  2011  2012   2013  2014  2015 2016  2017  2018  2019  2020 2021  2022
+NA 2.645 2.726 2.062 10.441 5.136 5.152 5.16 5.142 5.184 5.366 5.194 2.12 2.048
+    2023  2024
+NA 2.042 2.042
+
+svg(filename ="NZAL-allocation-all-baseline-720-540.svg", width = 8, height = 6, pointsize = 12, onefile = FALSE, family = "sans", bg = "white")
+png("NZAL-Allocation-all-baseline-560by420.png", bg="white", width=560, height=420,pointsize = 11)
+par(mar=c(4, 4, 4, 1)+0.1)
+barplot(nzalbaseline1,ylim=c(0,11),las=1,space=c(0.1,1.1), beside = TRUE, col=c(rep("#ED731D",14),"red"))
+title(cex.main=1.4,main=expression(paste("Aluminium all allocation baseline factors 2010 - 2024")),ylab="Units per tonne aluminium produced",xlab="")
+mtext(side=1,line=2.5,cex=1,expression(paste("Source: Climate Change (Eligible Industrial Activities) Regulations 2010 No 7")))
+mtext(side=3,line=0,cex=0.9,expression(paste("What happened in 2013? The allocation factor is five times more than emissions per tonne aluminium")))
+legend("topright", inset=c(0.0,0.0) ,bty="n",cex=1.2,c("Final allocation","Provisional allocation"),fill=c("#ED731D","red"))
+dev.off()
+
+https://www.legislation.govt.nz/regulation/public/2024/0028/latest/LMS944589.html
+2.042, which is the allocative baseline for any 2024 provisional allocation: 
+https://www.legislation.govt.nz/regulation/public/2023/0044/latest/LMS821749.html
+2.034, which is the allocative baseline for any 2023 provisional allocation:
+
+From the EPAs letter about calculating 2021 provisional allocation;  
+estimating the provisional allocation (PA) for 2023
+
+PA = LA x sum(PDCT x AB) / TM
+
+(S.81 https://www.legislation.govt.nz/act/public/2002/0040/latest/DLM1662643.html) 
+
+LA = level of assistance - 2010 to 2020 LA = 0.9 "less applicable phase out rate" (S.81.2.a) 0.01 p.a. from 2021 to 2030
+LA2023 <- 0.9 - 0.01 - 0.01 - 0.01 
+PDCT = "is the amount of each prescribed product from the eligible industrial activity produced by the person in the year immediately preceding the year to which the provisional allocation relates, as determined, if relevant, in accordance with regulations made under this Act"
+PDCT2023 = Final PDCT2022
+PDCT2023 <- nzal[nzal$Year =="2022" & nzal$Production,6] 
+AB2023 = 2.034
+#(Climate Change (Eligible Industrial Activities) Amendment Regulations 2023, https://www.legislation.govt.nz/regulation/public/2023/0044/latest/LMS821749.html
+TM = transitional measure (the 'two for one' to 2017) = 1
+ AB2023 = 2.034
+LA2023
+[1] 0.87
+PDCT2023
+[1] 295566.4
+AB2023
+[1] 2.034
+PA2023 <- round(LA2023 * (PDCT2023 * AB2023),0)
+PA2023 
+[1] 523028 
+tail(nzal,1)
+   Year  Units Mayunitprice Unitvalue Baseline Production
+13 2022 605320        76.55  46337246    2.048   295566.4
+2023 May mean unit price? => 53.81 to calculate value of units allocated in May 2023
+uv2023 <- round(53.81 * PA2023,2) 
+uv2023
+[1] 28144137
+rbind(nzal,
+pa2023 <- c(2023,PA2023,53.81,uv2023,AB2023,NA)
+pa2023
+[1]     2023.000   523028.000       53.810 28144136.680        2.034
+[6]           NA 
+
+nzal20102023 <- rbind(nzal, pa2023)
+tail(nzal20102023,3)
+   Year  Units Mayunitprice Unitvalue Baseline Production
+12 2021 628561        37.14  23344756    2.120   296491.0
+13 2022 605320        76.55  46337246    2.048   295566.4
+14 2023 523028        53.81  28144137    2.034         NA 
+# ceate factor denoting type of allocation, Final allocations to 2022 first
+Type <- rep("Final",13)
+Type <- append(Type,"Provisional")
+str(Type)
+Type
+[1] "Final"       "Final"       "Final"       "Final"       "Final"      
+ [6] "Final"       "Final"       "Final"       "Final"       "Final"      
+[11] "Final"       "Final"       "Final"       "Provisional" 
+# add to dataframe as a column
+nzal20102023$Type <- Type
+str(nzal20102023) 
+'data.frame':	14 obs. of  7 variables:
+ $ Year        : num  2010 2011 2012 2013 2014 ...
+ $ Units       : num  210421 437681 301244 1524172 755987 ...
+ $ Mayunitprice: num  17.58 19.84 6.23 1.94 4.08 ...
+ $ Unitvalue   : num  3699201 8683591 1876750 2956894 3084427 ...
+ $ Baseline    : num  2.65 2.73 2.06 10.44 5.14 ...
+ $ Production  : num  79554 160558 146093 145980 147194 ...
+ $ Type        : chr  "Final" "Final" "Final" "Final" ... 
+ 
+ 
+# How many NZUs were given to  New Zealand Aluminium Smelters Limited in total including the provisional allocation 2023?
+sum(nzal20102023[["Units"]])  
+[1] 12164601 
+# $12 million 
+# What was the market value of the NZUs given to  New Zealand Aluminium Smelters Limited in total the including provisional allocation 2023?
+sum(nzal20102023[["Unitvalue"]])  
+[1] 261199000 
+# $261 million 
+# Create a .csv formatted data file
+write.csv(nzal20102023, file = "nzalallocations", row.names = FALSE) 
+
+https://www.epa.govt.nz/industry-areas/emissions-trading-scheme/industrial-allocations/how-to-apply/    Step 2 choose an application type
+#Provisional If you apply for a Provisional Allocation you receive your entitlement in advance, based on your production for the previous calendar year. To ‘square up’ your entitlement with what you actually produced, in the next application period you’re required to submit an allocation adjustment.
+
+https://www.epa.govt.nz/industry-areas/emissions-trading-scheme/industrial-allocations/how-to-apply/    correct at 27/02/2022
+#Deadlines  info@epa.govt.nz
+#You must submit your application via the Register by the following statutory deadlines:
+#Provisional allocation applications: Apply between 1 January - 30 April of the year for which you wish to receive NZUs.
+#Final allocation applications and annual allocation adjustments: Apply between 1 January - 30 April of the year following the year for which you wish to receive NZUs.
